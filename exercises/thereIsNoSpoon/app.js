6b2c2781e6ec4beae5bbdb5f7ee3ea14d955e5a4
jameel <jameel@Vs-Mac-Pro-116.local>
2018-07-19 10:06:40 -0600

    Initial commit - catching up

---
+++ exercises/thereIsNoSpoon/app.js
@@ -0,0 +1,99 @@
+// 1)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 2)
+var phone = {  
+    color: "white",
+    price: 1500,
+    isIphone: true,
+    write: function() {
+        console.log("I'm a " + this.color + " phone that costs $" + this.price);
+    }
+}
+
+// 3)
+var monitor = {  
+    color: "silver",
+    price: 1500,
+    isMac: false,
+    write: function() {
+        console.log("I'm a " + this.color + " monitor that costs $" + this.price);
+    }
+}
+
+// 4)
+var headphones = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " headphone that costs $" + this.price);
+    }
+}
+
+// 5)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 6)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 7)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 8)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 9)
+var laptop = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}
+
+// 10)
+var coffee = {  
+    color: "silver",
+    price: 1500,
+    isMac: true,
+    write: function() {
+        console.log("I'm a " + this.color + " mac that costs $" + this.price);
+    }
+}

b2631dfdf67b1c99bfa3c808196f031480227f3b
jameel <jameel.matin@gmail.com>
2018-11-05 15:59:10 -0800

    initial commit

--- exercises/ng-app/app.js
+++ exercises/ng-app/app.js
@@ -1,5 +1,18 @@
-var myApp = angular.module('myApp', [])
+let myApp = angular.module("myApp", []);
 
-myApp.controller('mainController', function(){
-
+myApp.controller("mainController", function($scope, $log) {
+  // console.log('$scope', $scope, '$log', $log);
+  $log.log("hello");
 });
+
+let Buisiness = function(name, size) {
+  this.name = name;
+  this.size = size;
+};
+
+function logBuisiness(buisiness) {
+  console.log("this is buisiness", buisiness);
+}
+
+let target = new Buisiness("Target", 12);
+logBuisiness(target);
